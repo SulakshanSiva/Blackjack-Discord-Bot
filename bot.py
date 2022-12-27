2d30a7dd18bf3b75d9e81cf2cead40e1237f0fbe
Sulakshan <sulakshansivak@gmail.com>
2022-12-26 21:42:48 -0500

    created profile command

--- bot.py
+++ bot.py
@@ -14,8 +14,21 @@ async def on_ready():
     print("Bot is online")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.reply("Hi")
+async def profile(ctx, member:discord.Member = None):
+    if member == None:  
+        member = ctx.author
+
+    name = member.display_name
+    pfp = member.display_avatar
+
+    embed = discord.Embed(title="Profile", description="Player Stats", colour=discord.Colour.random())
+    embed.set_author(name=f"{name}")
+    embed.set_thumbnail(url=f"{pfp}")
+    embed.add_field(name="Coins", value="test", inline=False)
+    embed.add_field(name="Wins", value="test", inline=True)
+    embed.add_field(name="Losses", value="test", inline=True)
+
+    await ctx.send(embed=embed)
 
 
 bot.run(token)
